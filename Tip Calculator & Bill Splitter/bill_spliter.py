e63eb43135fff8fa429ff250550cdb9dc1911a85
dev-sereti <devsereti@gmail.com>
2025-12-19 12:05:00 +0300

    Added inputs

--- Tip Calculator & Bill Splitter/bill_spliter.py
+++ Tip Calculator & Bill Splitter/bill_spliter.py
@@ -29,3 +29,7 @@ class TipSplitterApp(tk.Tk):
         pad = 10
         main = ttk.Frame(self, padding=pad)
         main.grid(row=0, column=0, sticky="nsew")
+        
+        
+        ttk.Label(main,text="Bill Amount :").grid(row=0,column=0,sticky="w")
+        self.bill_var = tk.StringVar(value=)
