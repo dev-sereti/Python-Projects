7399d2f86d7831a647053554d39d9d17d2f9b9a7
dev-sereti <devsereti@gmail.com>
2025-12-19 10:06:56 +0300

    Created a function to convert text to decimal

--- Tip Calculator & Bill Splitter/bill_spliter.py
+++ Tip Calculator & Bill Splitter/bill_spliter.py
@@ -2,3 +2,7 @@ import tkinter as tk
 from tkinter ttk,messagebox
 from decimal import Decimal,InvalidOperation,ROUND_HALF_UP
 
+MONEY_PLACES = Decimal(("0.01"))
+
+# Convert to decimal
+def to_decimal (text:str) -> Decimal:
