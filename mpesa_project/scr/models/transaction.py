ac2a0f90f6b7a400c5ab61875b7b779e53f4ce28
dev-sereti <devsereti@gmail.com>
2025-12-30 12:03:33 +0300

    detect_transaction_type function added.

--- mpesa_project/scr/models/transaction.py
+++ mpesa_project/scr/models/transaction.py
@@ -5,17 +5,25 @@ from typing import Optional
 @dataclass
 class Transaction:
     """M-Pesa transaction data model"""
-    transaction_code:str
-    amount:float
-    date:datetime
-    fee:float
-    transaction_type:str # Sent, Received, Withdrawn, etc.
+    transaction_code: str
+    amount: float
+    date: datetime
+    fee: float
+    transaction_type: str  # Sent, Received, Withdrawn, etc.
+    recipient: Optional[str] = None
+    sender: Optional[str] = None
+    balance: Optional[float] = None
+    message: Optional[str] = None
 
     def to_dict(self):
         return{
-           'transaction_code':self.transaction_code,
-           'amount:float':self.amount,
-           'date:datetime':self.date.strftime('%Y-%m-%d %H:%M:%S'),
-           'fee:float':self.fee,
-           'transaction_type:str':self.transaction_type
+            'transaction_code': self.transaction_code,
+            'amount': self.amount,
+            'date': self.date.strftime('%Y-%m-%d %H:%M:%S'),
+            'fee': self.fee,
+            'transaction_type': self.transaction_type,
+            'recipient': self.recipient,
+            'sender': self.sender,
+            'balance': self.balance,
+            'message': self.message
         }
