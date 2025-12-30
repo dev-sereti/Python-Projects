48a3be833e638d98b4ba5fd0b9c9d2a2487fb418
dev-sereti <devsereti@gmail.com>
2025-12-30 13:26:28 +0300

    test_parser  error fixed.

--- mpesa_project/tests/test_parser.py
+++ mpesa_project/tests/test_parser.py
@@ -28,16 +28,19 @@ class TestMPesaParser(unittest.TestCase):
         # self.assertEqual(transaction.fee, 15.00)
         # self.assertEqual(transaction.transaction_type, 'sent')
     
-    def test_received_money(self):
-        message = """RBK9876543 Confirmed. You have received Ksh1,000.00 from 
-        JANE SMITH 254723456789 on 15/1/24 at 3:45 PM. 
-        New M-PESA balance is Ksh3,500.00"""
-        
-        transaction = self.parser.parse_message(message)
-        
-        self.assertIsNotNone(transaction)
-        self.assertEqual(transaction.amount, 1000.00)
-        self.assertEqual(transaction.transaction_type, 'received')
+def test_received_money(self):
+    message = """RBK9876543 Confirmed. You have received Ksh1,000.00 from 
+    JANE SMITH 254723456789 on 15/1/24 at 3:45 PM. 
+    New M-PESA balance is Ksh3,500.00"""
+    
+    transaction = self.parser.parse_message(message)
+    
+    self.assertIsNotNone(transaction)
+    assert transaction is not None
+
+    self.assertEqual(transaction.amount, 1000.00)
+    self.assertEqual(transaction.transaction_type, 'received')
+
     
 if __name__ == '__main__':
     unittest.main()
