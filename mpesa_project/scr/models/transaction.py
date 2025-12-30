25bad589371f1298045f9760bd93baae3d7683ba
dev-sereti <devsereti@gmail.com>
2025-12-30 11:14:19 +0300

    transaction class to transaction.py

--- mpesa_project/scr/models/transaction.py
+++ mpesa_project/scr/models/transaction.py
@@ -1,3 +1,21 @@
 from dataclasses import dataclass
 from datetime import  datetime
 from typing import Optional
+
+@dataclass
+class transaction:
+    """M-Pesa transaction data model"""
+    transaction_code:str
+    amount:float
+    date:datetime
+    fee:float
+    transaction_type:str # Sent, Received, Withdrawn, etc.
+
+    def to_dict(self):
+        return{
+           'transaction_code:str':self.transaction_code,
+           'amount:float':self.amount,
+           'date:datetime':self.date,
+           'fee:float':self.fee,
+           'transaction_type:str':self.transaction_type
+        }
